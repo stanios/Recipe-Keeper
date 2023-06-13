8b8a351a812ecc72b730c86549038e48ed8f5b49
G. Staniogloudis <geo.stanios@gmail.com>
2023-06-13 19:59:37 +0300

    HashlocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +31,8 @@ import { Sharedmodule } from './shared/shared.module';
     RecipeService, 
     {provide: HTTP_INTERCEPTORS, 
      useClass: AuthInterceptorService,
-     multi: true}],
+     multi: true},
+     { provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
